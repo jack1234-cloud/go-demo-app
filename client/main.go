10429f241c968046286060850d54d4610157a669
joflynn <jack.oflynn@aig.com>
2022-02-23 13:00:01 +0000

    updates

--- client/main.go
+++ client/main.go
@@ -1,5 +1,18 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
+func main() {
+	//Plain URL
+	response, err := http.Get("http://localhost/")
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		data, _ := ioutil.ReadAll(response.Body)
+		fmt.Println(string(data))
+	}
 }
